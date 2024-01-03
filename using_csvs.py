9aeebd09c999ef5b1a55b7aa74925164bb38bd0a
bhaskar <mallu.bhaskar@yahoo.com>
2024-01-03 22:14:09 +0530

    Modified the code to read csv

--- using_csvs.py
+++ using_csvs.py
@@ -4,4 +4,13 @@ with open('data/employee_birthday.csv') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}')
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}')
+            line_count += 1
+        
+    print(f'Processed line count {line_count} lines')
+
+        
